6696067e38c0c788519b6bacea0179eb26dbf6de
YinghaoFu <120256627+fuyinghao1998@users.noreply.github.com>
2023-12-16 17:53:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
   version = '0.0.1',      # add new feature: PAI method
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Tool code collection (for personal use)',
+  long_description = file: README.md
+  long_description_content_type = text/markdown
   author = 'Fu Yinghao',                   # Type in your name
   author_email = 'yhfu1998@163.com',      # Type in your E-Mail
   url = 'https://github.com/fuyinghao1998/FuToolbox',   # Provide either the link to your github or to your website
